36775d3204609bc35d89c30df0eb869febeefe6f
Quy <Quynvhe180901@fpt.edu.vn>
2026-02-21 21:49:44 +0700

    package setup

---
+++ src/main/java/com/itserviceflow/ITServiceFlow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itserviceflow;
+
+/**
+ *
+ * @author Admin
+ */
+public class ITServiceFlow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
